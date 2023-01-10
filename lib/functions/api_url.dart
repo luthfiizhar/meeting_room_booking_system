fea8dfec0c7dca67ad8597e5aba67f61a69b3758
luthfiizhar <59906471+luthfiizhar@users.noreply.github.com>
2023-01-10 12:48:05 +0700

    Update api_url.dart

--- lib/functions/api_url.dart
+++ lib/functions/api_url.dart
@@ -1,2 +1,2 @@
-const String apiUrlGlobal = 'fmklg.klgsys.com'; // Development
-//const String apiUrlGlobal = 'fmklg-backend.klgsys.com'; // Production
+// const String apiUrlGlobal = 'fmklg.klgsys.com'; // Development
+const String apiUrlGlobal = 'fmklg-backend.klgsys.com'; // Production
